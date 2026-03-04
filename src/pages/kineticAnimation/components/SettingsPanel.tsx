8abfed4db40d91fa63b3289816dea4a68fc9870b
AbramovichCp <abramovich.cp@gmail.com>
2026-03-04 22:35:38 +0100

    fix panel

--- src/pages/kineticAnimation/components/SettingsPanel.tsx
+++ src/pages/kineticAnimation/components/SettingsPanel.tsx
@@ -90,7 +90,7 @@ export function SettingsPanel({
 
       <Separator />
 
-      <ScrollArea className="flex-1">
+      <ScrollArea className="flex-1 min-h-0">
         <div className="flex flex-col gap-6 p-4">
           {SECTIONS.map((section, si) => (
             <div key={section.name}>
